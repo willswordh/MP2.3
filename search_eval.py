ca99721c897bd4ec12f555a644f241c72bcb7888
Qiwei He <qiweihe@Qiweis-MacBook-Pro.local>
2022-09-25 22:01:36 -0500

    code

--- search_eval.py
+++ search_eval.py
@@ -20,10 +20,10 @@ class InL2Ranker(metapy.index.RankingFunction):
         For fields available in the score_data sd object,
         @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
         """
-        tfn = sd.doc_term_count * math.log((1+ sd.avg_dl/sd.doc_size,2)
+        tfn = sd.doc_term_count * math.log(1 + sd.avg_dl/sd.doc_size,2)
      
         
-        return  (sd.query_term_weight *(tfn/(tfn + self.param)) * math.log((sd.num_docs +1)/(sd.corpus_term_count +0.5)),2)
+        return  sd.query_term_weight *(tfn/(tfn + self.param)) * math.log((sd.num_docs +1)/(sd.corpus_term_count +0.5),2)
 
 
 def load_ranker(cfg_file):
